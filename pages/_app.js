f9c5031dfa8090282f0d3a391c59b1c71157d24b
rifaamaliah <rifatulamaliah5@gmail.com>
2022-01-20 11:18:16 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <Head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"/>
+    </Head>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
+
